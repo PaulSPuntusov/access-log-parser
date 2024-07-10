e3c47df0df36d70d3dac597440845bc8d4ad4f30
PaulSPuntusov <pps_ru@mail.ru>
2024-07-10 21:35:52 +0300

    Четвертая домашка - реализация isPositive

---
+++ src/IsPositive.java
@@ -0,0 +1,11 @@
+public class IsPositive {
+    public static boolean isPositive(int x){
+        boolean res;
+        if (x>0){
+            res = true;
+        } else {
+            res = false;
+        }
+        return res;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
-        double quotient = (double)firstNumber/secondNumber;
-        System.out.println("Сумма этих двух чисел равна "+(firstNumber+secondNumber));
-        System.out.println("Разность этих двух чисел равна "+(firstNumber-secondNumber));
-        System.out.println("Произведение этих двух чисел равно "+(firstNumber*secondNumber));
-        System.out.println("Частное этих двух чисел равно "+quotient);
+        System.out.println(IsPositive.isPositive(3));
+        System.out.println(IsPositive.isPositive(-5));
     }
 }
